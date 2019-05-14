8fa3a7abd907b73518ca44f2b5a96e79525fe793
Tatev Grigoryan <grigoryantatev1990@gmail.com>
2019-05-14 12:02:33 +0400

    homework 5

--- HomeWork5.js
+++ HomeWork5.js
@@ -4,29 +4,30 @@ first element and returns the given array. (without using
 arr.unshift(),assign second element to first, third element to
 second...)*/
 
-function getArrWithoutFirstElem(arr, res=[]){
-	if(arr.length <= 1){
-		return [];
-	}
-	res.push(arr[1]);
-	getArrWithoutFirstElem(arr.splice(1), res);
-	return res;
+function getArrWithoutFirstElem(arr, res = []) {
+    if (arr.length <= 1) {
+        return [];
+    }
+    res.push(arr[1]);
+    getArrWithoutFirstElem(arr.splice(1), res);
+    return res;
 }
 
 //homework 5.2
 /*Given an array of nested arrays. Write a recursive function
 that flattens it. (Hint create function that concats arrays).*/
-function concatArray(arr, res = []){
-	if(arr.length === 0){
-		return arr;
-	}
-if(Array.isArray(arr[0])){
- concatArray(arr[0],res);
-}else{
-res.push(arr[0]);
-}
-res.concat(concatArray(arr.splice(1),res));
-return res;;
+function concatArray(arr, res = []) {
+    if (arr.length === 0) {
+        return arr;
+    }
+    if (Array.isArray(arr[0])) {
+        concatArray(arr[0], res);
+    } else {
+        res.push(arr[0]);
+    }
+    res.concat(concatArray(arr.splice(1), res));
+    return res;
+    ;
 }
 
 
@@ -36,15 +37,15 @@ the digits and if that sum has more than 1 digit find the sum of
 digits of that number. Repeat that process if needed and return
 the result.*/
 
-function singleSumOfDigits(num){
-while( num >= 10 ){
-let sum = 0;
-	for(let i = 0; i < String(num).length; i++){
-		sum += +String(num)[i]
+function singleSumOfDigits(num) {
+    while (num >= 10) {
+        let sum = 0;
+        for (let i = 0; i < String(num).length; i++) {
+            sum += +String(num)[i]
+        }
+        num = sum;
     }
-	num = sum;
-   }
-return num;
+    return num;
 }
 
 
@@ -53,13 +54,13 @@ return num;
 function that rotates an array N places to the left. (Hint: to
 add element to the beginning use arr.unshift())*/
 
-function arrayRotate(arr, num){
-	if(num === arr.length || num === 0){
-		return arr;
-	}
-	arr.unshift(arr[arr.length-1]);
-	arr.pop(arr[arr.length-1]);
-	return arrayRotate(arr, ++num);
+function arrayRotate(arr, num) {
+    if (num === arr.length || num === 0) {
+        return arr;
+    }
+    arr.unshift(arr[arr.length - 1]);
+    arr.pop(arr[arr.length - 1]);
+    return arrayRotate(arr, ++num);
 }
 
 
@@ -68,28 +69,27 @@ function arrayRotate(arr, num){
 become keys). If there is more than key for that given value
 create an array.*/
 
- function getNewObject(obj){
-	let res = {};
-	outer: for(objkey in obj){
- 	debugger;
- 		for(reskey in res){
-			if(reskey === String(obj[objkey])){
-			
-			if(Array.isArray(res[reskey])){
-				res[reskey].push(objkey);
-			}else{
-				let ar1=[];
-				ar1.push(res[reskey]);
-				ar1.push(objkey);
-				res[obj[objkey]] = ar1;
+function getNewObject(obj) {
+    let res = {};
+    outer: for (objkey in obj) {
+        for (reskey in res) {
+            if (reskey === String(obj[objkey])) {
+
+                if (Array.isArray(res[reskey])) {
+                    res[reskey].push(objkey);
+                } else {
+                    let ar1 = [];
+                    ar1.push(res[reskey]);
+                    ar1.push(objkey);
+                    res[obj[objkey]] = ar1;
+                }
+                continue outer;
             }
-   			continue outer;
-			}
-		}
-		res[obj[objkey]] = objkey;
-	}
-return res;
- }
+        }
+        res[obj[objkey]] = objkey;
+    }
+    return res;
+}
 
 
 //homework 5.6
@@ -102,9 +102,11 @@ return res;
 { book: &quot;After Dark&quot;, readStatus: true, percent: 70 }
 ];
 Output the books sorted by the percent in descending order which readStatus is true.*/
-function listSort(arrlist){
-debugger;
-let res = arrlist.filter( index => index.readStatus === true );
-res = res.sort( (a,b)=> { return b.percent - a.percent} );
-return res;
+function listSort(arrlist) {
+    let res = arrlist.filter(index => index.readStatus === true);
+    res = res.sort((a, b) => {
+        return b.percent - a.percent
+    });
+    return res;
 }
+
